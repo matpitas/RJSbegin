6c16c234e6cff8cd64b62b43e8efa2db5e341eef
matpitas <pitas.matheus4@gmail.com>
2022-08-30 09:51:23 -0300

    commit

---
+++ reactapp/src/Components/Card/card.jsx
@@ -0,0 +1,10 @@
+import './card.css'
+
+export function Card({name,time}){
+    return(
+        <div className="card">
+            <strong>{name}</strong>
+            <small>{time}</small>
+        </div>
+    )
+}
---
+++ reactapp/src/pages/Home/Home.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import './home.css'
+import { Card } from '../../Components/Card/card'
+
+export function Home() {
+  
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: '', avatar: ''})
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-BR",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+    
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+     async function fetchData() {
+      const response = await fetch("https://api.github.com/users/matpitas")
+      const data = await response.json()
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+
+     }
+
+     fetchData()
+  }, [])
+
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+
+        <div className="">
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+
+      </header>
+      <input
+      type="text"
+      placeholder="Digite o nome" 
+      onChange={e => setStudentName(e.target.value)}
+      />
+      <button onClick={handleAddStudent}>Enviar</button>
+
+      {
+      students.map((student) => (
+        <Card 
+          key={student.time}
+          name={student.name} 
+          time={student.time} 
+        />
+        ))
+      }
+
+
+
+    </div>
+  )
+}
+
